59b72fc6e0e94b5eef0103e7dcfa15f3875ed825
Haley Register <haleyreg@gmail.com>
2020-04-22 13:04:26 -0500

    completed project, and all tests passed

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PalindromeExercise
+{
+    public class Palindrome
+    {
+        public bool IsAPalindrome(string str1)
+        {
+            string str2;
+            char[] ch = str1.ToCharArray();
+
+            Array.Reverse(ch);
+            str2 = new string (ch);
+
+            bool b = str1.Equals(str2, StringComparison.OrdinalIgnoreCase);
+            if(b == true)
+            {
+                 return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,27 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("hello", false)]
+        [InlineData("sugar", false)]
+
+        public void PalindromeTest (string word, bool expected)
         {
+            //arrange
+            var sut = new Palindrome();
+
+            //act
+            var actual = sut.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(actual, expected);
 
         }
     }
